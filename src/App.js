633f16f750d12c7963948c20d1d524ea8eb20cb8
Sivaraman-skcet <151620897+Sivaraman-skcet@users.noreply.github.com>
2023-11-21 16:28:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Timer from './timer';
+
+const App = () => {
+  return (
+    <div>
+      <Timer initialSeconds={0} />
+    </div>
+  );
+};
+
+export default App;
